cae758f2c89b7b5e9cfb6f7a84478129a9c76343
Ismail Sunni <imajimatika@gmail.com>
2024-01-28 02:41:02 +0700

    Add build to gh-pages.

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/openlayers-examples/", // Replace 'repository-name' with your actual repository name
+  // ... other configurations
+});
